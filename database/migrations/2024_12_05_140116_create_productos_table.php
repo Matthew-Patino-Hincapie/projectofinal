77977127acc4065ac06df19836be5f857116d4d7
Matthew Patiño Hincapie <megamatthewhp@gmail.com>
2024-12-06 23:08:42 -0500

    trabajo final de laravel

---
+++ database/migrations/2024_12_05_140116_create_productos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre',30)->unique();
+            $table->string('descripcion',30)->unique();
+            $table->foreignId('id_categoria')->constrained('categorias')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('id_proveedor')->constrained('proveedors')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};
